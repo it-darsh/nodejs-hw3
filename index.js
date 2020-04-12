0cfdbd67f9be72a7d555c3228eb18a787a3b91be
it-darsh <github.com@darsh.ru>
2020-04-12 15:03:19 +0300

    добавил начальную обработку формы /login

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', require('./routes/index'));
--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,12 @@ router.get('/admin', (req, res) => {
   res.render('pages/admin');
 });
 
+router.post('/login', function(req, res) {
+  if (!req.body.password || !req.body.email) {
+    return res.send('404');
+  }
+  // return res.send(`EMail - ${req.body.email} Password: ${req.body.password}`);
+  res.redirect('/admin');
+});
+
 module.exports = router;
