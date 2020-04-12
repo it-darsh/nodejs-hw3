71a609c4629dc454af31ad5966d5b2bfc56ad989
it-darsh <github.com@darsh.ru>
2020-04-12 15:44:35 +0300

    простая авторизация. сессия 10 секунд

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const session = require('express-session');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -12,7 +12,27 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(
+  session({
+    secret: 'loftschool',
+    key: 'sissionkey',
+    cookie: {
+      path: '/',
+      httpOnly: true,
+      maxAge: 10000
+    },
+    saveUninitialized: false,
+    resave: false
+  })
+);
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', (req, res, next) => {
+  req.session.views = req.session.views === void 0 ? 0 : ++req.session.views
+  next()
+})
+
 app.use('/', require('./routes/index'));
 
 app.listen(3000, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+
+const isAdmin = (req, res, next) => {
+  if(req.session.isAdmin) {
+    return next();
+  }
+  res.redirect('/login');
+};
 
 router.get('/', (req, res) => {
   res.render('pages/index');
@@ -9,16 +15,17 @@ router.get('/login', (req, res) => {
   res.render('pages/login');
 });
 
-router.get('/admin', (req, res) => {
-  res.render('pages/admin');
+router.post('/login', (req, res) => {
+  //проверка связки логин-пароль
+  if (!req.body.password || !req.body.email) {
+    res.render('pages/login');
+  }
+  req.session.isAdmin = true;
+  res.redirect('/admin');
 });
 
-router.post('/login', function(req, res) {
-  if (!req.body.password || !req.body.email) {
-    return res.send('404');
-  }
-  // return res.send(`EMail - ${req.body.email} Password: ${req.body.password}`);
-  res.redirect('/admin');
+router.get('/admin', isAdmin, (req, res) => {
+  res.render('pages/admin');
 });
 
 module.exports = router;
