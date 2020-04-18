80b2cb3c54522c5eb545e70cbf5de6e7c7af25db
it-darsh <github.com@darsh.ru>
2020-04-18 17:34:48 +0300

    добавил добавление товаров

--- db.js
+++ db.js
@@ -9,6 +9,29 @@ const dbCRUD = {
     return db.get(table)
       .value();
   },
+  set: (table, arr) => {
+    if(arr[0]) {
+      db.set(table+'[0].number', Number(arr[0]))
+        .write()
+    }
+    if(arr[1]) {
+      db.set(table+'[1].number', Number(arr[1]))
+        .write()
+    }
+    if(arr[2]) {
+      db.set(table+'[2].number', Number(arr[2]))
+        .write()
+    }
+    if(arr[3]) {
+      db.set(table+'[3].number', Number(arr[3]))
+        .write()
+    }
+  },
+  add: (table, obj) => {
+    db.get(table)
+      .push(obj)
+      .write()
+  },
 }
 
 module.exports = dbCRUD;
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const db = require('../db');
+const formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
 
 const isAdmin = (req, res, next) => {
   if(req.session.isAdmin) {
@@ -41,14 +44,55 @@ router.get('/admin', isAdmin, (req, res) => {
 });
 
 router.post('/admin/upload', (req, res) => {
+  let form = new formidable.IncomingForm();
+  let upload = path.join('./public', 'assets', 'img', 'products')
 
+  if (!fs.existsSync(upload)) {
+    fs.mkdirSync(upload)
+  }
+
+  form.uploadDir = path.join(process.cwd(), upload)
+
+  form.parse(req, function (err, fields, files) {
+    // if (err) {
+    //   return next(err)
+    // }
+
+    // const valid = validation(fields, files)
+
+    // if (valid.err) {
+    //   fs.unlinkSync(files.photo.path)
+    //   return res.redirect(`/?msg=${valid.status}`)
+    // }
+
+    const fileName = path.join(upload, files.photo.name)
+
+    fs.rename(files.photo.path, fileName, function (err) {
+      if (err) {
+        console.error(err.message)
+        return
+      }
+
+      let dir = fileName.substr(fileName.indexOf('\\'))
+
+      // db.set(fields.name, dir)
+      // db.save()
+      db.add('products', {src: dir, name: fields.name, price: fields.price })
+      console.log(fields.name, fields.price, dir);
+      res.redirect('/admin?msg=Картинка успешно загружена')
+    })
+  })
 });
 
-router.post('/admin/skills', (req, res) => {
-  if (!req.body.age || !req.body.concerts || !req.body.cities || !req.body.years) {
-    return res.render('pages/admin');
+router.post('/admin/skills', isAdmin, (req, res) => {
+  const age = (req.body.age) ? req.body.age : undefined;
+  const concerts = (req.body.concerts) ? req.body.concerts : undefined;
+  const cities = (req.body.cities) ? req.body.cities : undefined;
+  const years = (req.body.years) ? req.body.years : undefined;
+
+  if(age || concerts || cities || years) {
+    db.set('skills',[age, concerts, cities, years]);
   }
-  console.log('post: /admin/skills', `2Age: ${req.body.age} Concert: ${req.body.concerts} Cities: ${req.body.cities} Year: ${req.body.years}`);  
   res.render('pages/admin');
 });
 
